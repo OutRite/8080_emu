9c9a2b87f60591c4f106b5bf26cf03a7d59afe55
OutRite <usless534@protonmail.com>
2022-11-17 01:56:58 -0800

    Add information to README, remove unnecessary main.py code More specifically, we were originally modifying cpudiag, which seems unnecessary.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Space Invaders Emulator
- A Space Invaders arcade emulator that doesn't work.
+ A Space Invaders arcade emulator that doesn't work.  
+It also contains some (small) CP/M HLE code to use with cpudiag.
+## Controls
+1p left: left arrow key  
+1p right: right arrow key  
+insert coin: q  
+1p start: a  
+1p shoot: z  
+2p left: j  
+2p right: l  
+2p start: s  
+2p shoot: m  
+tilt: t
--- main.py
+++ main.py
@@ -11,13 +11,6 @@ memory.load_file(0x1000, "invaders.f")
 memory.load_file(0x1800, "invaders.e")
 
 #memory.load_file(0x0100, "cpudiag.bin")
-#memory.write_memory(0x0000, 0xc3, restricted=False)
-#memory.write_memory(0x0001, 0x00, restricted=False)
-#memory.write_memory(0x0002, 0x01, restricted=False)
-#memory.write_memory(368, 0x07, restricted=False)
-#memory.write_memory(0x059c, 0xc3, restricted=False)
-#memory.write_memory(0x059d, 0xc2, restricted=False)
-#memory.write_memory(0x059e, 0x05, restricted=False)
 #cpu.display_enabled = False
 #opcodes.cpm_mode = True
 
